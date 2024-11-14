b939bfd354570e45f70341d548a363a4c4e0ef3c
cloud2312 <cloudexpert2312@gmail.com>
2024-11-14 20:48:49 +0530

    Third Commit

---
+++ newtesting/New-module-testing/module/security/ec2-instance.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dbed73e23e7c95377dc6dfbf5ff021348971f8a39f08630299e9601ab450529a
+size 4486
---
+++ newtesting/New-module-testing/module/security/securitygroup-rule.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2f816669201c0bd4619f680821eb8eb950e45a7583cd0708bab5d14717838a04
+size 3361
